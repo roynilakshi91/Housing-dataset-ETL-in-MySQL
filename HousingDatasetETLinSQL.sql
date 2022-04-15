c8f7ee74d059be5f2b394bbc0a2dc109f6320bba
roynilakshi91 <82709191+roynilakshi91@users.noreply.github.com>
2022-04-15 01:40:46 -0700

    Update HousingDatasetETLinSQL.sql

--- HousingDatasetETLinSQL.sql
+++ HousingDatasetETLinSQL.sql
@@ -1,13 +1,13 @@
-#Cleaning data in SQL Queries
+Cleaning data in SQL Queries
 
 
-1. #Viewing the data using NILAKSHIDB database
+1. Viewing the data using NILAKSHIDB database
 
 SELECT * FROM NILAKSHIDB.housingdataseforetl
 
 
 
-3. #Standardize Sales Date format
+3. Standardize Sales Date format
 
 USE NILAKSHIDB;
 SET SQL_SAFE_UPDATES = 0;
@@ -17,8 +17,7 @@ Select saledate FROM NILAKSHIDB.housingdataseforetl
 
 
 
-4. #Populate Property Address
-   #Doing self join on  NILAKSHIDB.`HousingDataForETL`
+4. Populate Property Address. Doing self join on  NILAKSHIDB.`HousingDataForETL`
    
 SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress ,IFNULL(a.PropertyAddress,b.PropertyAddress)
 FROM NILAKSHIDB.`housingdataseforetl` a
@@ -29,7 +28,7 @@ where a.PropertyAddress IS NULL
 
 
 
-# Updating it in the Database
+--Updating it in the Database
 
 UPDATE housingdataseforetl as a JOIN housingdataseforetl as b 
     ON a.ParcelID = b.ParcelID
@@ -40,36 +39,37 @@ WHERE a.PropertyAddress IS NULL;
 
 
 
-5. #Separating the Address into adress, city, state
+5. Separating the Address into adress, city, state
 
 Select PropertyAddress from NILAKSHIDB.housingdataseforetl
 
 
-#USE CHARINDEX insteasd of LOCATE in SQL SERVER
-#USE LEN insteasd of CHAR_LENGTH in SQL SERVER
+--USE CHARINDEX insteasd of LOCATE in SQL SERVER
+--USE LEN insteasd of CHAR_LENGTH in SQL SERVER
 
 Select SUBSTRING(PropertyAddress,1, LOCATE(",",PropertyAddress)-1) AS Address,
 SUBSTRING(PropertyAddress, LOCATE(",",PropertyAddress)+1, CHAR_LENGTH(PropertyAddress)) AS Address
 from NILAKSHIDB.housingdataseforetl
 
 
-#Creating a new column as Street_Address
+--Creating a new column as Street_Address
 ALTER TABLE housingdataseforetl
 ADD Street_Address varchar(255);
 Update housingdataseforetl SET Street_Address = SUBSTRING(PropertyAddress,1, LOCATE(",",PropertyAddress)-1)
 
 
-#Creating a new column as City_new
+--Creating a new column as City_new
 ALTER TABLE housingdataseforetl
 ADD City_new varchar(255);
 Update housingdataseforetl SET City_new = SUBSTRING(PropertyAddress, LOCATE(",",PropertyAddress)+1, CHAR_LENGTH(PropertyAddress))
 
-#Checking table with two new updated column
+--Checking table with two new updated column
 Select * FROM NILAKSHIDB.housingdataseforetl
 
 
 
-6. #Doing the same with Owner Address
+6. Doing the same with Owner Address
+
 Select
 SUBSTRING_INDEX(REPLACE(OwnerAddress, ",","."),".",-1),
 SUBSTRING_INDEX(REPLACE(OwnerAddress, ",","."),".",-2),
@@ -78,31 +78,30 @@ FROM NILAKSHIDB.housingdataseforetl
 
 
 
-#Creating New State column in the table from Owner Address
+--Creating New State column in the table from Owner Address
 ALTER TABLE housingdataseforetl
 ADD ownerAddress_state varchar(255);
 Update housingdataseforetl SET ownerAddress_state = SUBSTRING_INDEX(REPLACE(OwnerAddress, ",","."),".",-1)
 
 
-#Creating New City column in the table from Owner Address
+--Creating New City column in the table from Owner Address
 ALTER TABLE housingdataseforetl
 ADD ownerAddress_city varchar(255);
 Update housingdataseforetl SET ownerAddress_city = SUBSTRING_INDEX(REPLACE(OwnerAddress, ",","."),".",-2)
 
 
-#Creating New Street column in the table from Owner Address
+--Creating New Street column in the table from Owner Address
 ALTER TABLE housingdataseforetl
 ADD ownerAddress_street varchar(255);
 Update housingdataseforetl SET ownerAddress_street = SUBSTRING_INDEX(REPLACE(OwnerAddress, ",","."),".",1)
 
 
-# Look at the table
+-- Look at the table
 Select * FROM NILAKSHIDB.housingdataseforetl
 
 
 
-7. # Change Y and N to Yes and No to "Sold as Vacant" field
-   #Finding the count and unique label of yes and no in "sold as vacant" field
+7. Change Y and N to Yes and No to "Sold as Vacant" field. Finding the count and unique label of yes and no in "sold as vacant" field
    
 Select Distinct(SoldASvacant), count(soldasvacant)
 FROM NILAKSHIDB.housingdataseforetl
@@ -118,14 +117,14 @@ CASE WHEN soldasvacant = "Y" THEN "YES"
      FROM NILAKSHIDB.housingdataseforetl
      
      
-#Updating it in the table    
+--Updating it in the table    
 
 Update housingdataseforetl SET soldasvacant = CASE WHEN soldasvacant = "Y" THEN "YES"
      WHEN soldasvacant = "N" THEN "NO"
      ELSE Soldasvacant
      END
 
-#Changes have been made    
+--Changes have been made    
 
 Select Distinct(SoldASvacant)
 FROM NILAKSHIDB.housingdataseforetl
@@ -137,8 +136,7 @@ order by 2
 
 
 
-8. #Remove the duplicates
-   #Finding duplicate rows using CTE (Common Table Expression) and Row_Number
+8. Remove the duplicates. Finding duplicate rows using CTE (Common Table Expression) and Row_Number
    
    
    
@@ -151,7 +149,7 @@ where rownum>1
 
 
 
-#Delete this duplicate rows from the table
+--Delete this duplicate rows from the table
 
 WITH CTE AS(Select * ,
 ROW_NUMBER() OVER(PARTITION BY ParcelID, propertyaddress, saleprice, saledate,legalreference ORDER BY parcelId) AS rownum
@@ -162,8 +160,8 @@ where CTE.rownum>1
 
 
 
-#Removing unused columns from table
-#Deleting OwnerAddress and PropertyAddress and TaxDistrict from the table
+--Removing unused columns from table
+--Deleting OwnerAddress and PropertyAddress and TaxDistrict from the table
 
 ALTER TABLE housingdataseforetl
 DROP COLUMN OwnerAddress
